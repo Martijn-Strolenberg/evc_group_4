78100880b60e3154d53d92dc3f2da60208ee2c9a
Luka Tepavcevic <l.tepavcevic@student.tue.nl>
2025-05-13 15:07:35 +0200

    Co-authored-by: Karsten <Karsten10@users.noreply.github.com> Co-authored-by: Sweeger <Sweeger@users.noreply.github.com> Co-authored-by: Martijn Strolenberg <Martijn-Strolenberg@users.noreply.github.com> Co-authored-by: SteveDiks <SteveDiks@users.noreply.github.com>

--- workshops/workshop3_group4/src/motor_control/src/odometry_node.py
+++ workshops/workshop3_group4/src/motor_control/src/odometry_node.py
@@ -1,18 +1,9 @@
-
-import time
-import hat
-from hat import *
-
-import cv2
 import rospy
 import numpy as np
 
-from sensor_msgs.msg import CompressedImage
-from jetson_camera.msg import twovids
-from motor_control.msg import motor_cmd
-from motor_odometry.msg import odem
-from nav_msgs.msg
-
+from motor_control.msg import motor_cmd, encoder
+from nav_msgs.msg import Odometry # maybe for future use
+from encoderDriver import *
 
 class OdometryPublisherNode:
     def __init__(self):
@@ -20,39 +11,93 @@ class OdometryPublisherNode:
         rospy.loginfo("Initializing odometry node...")
         self.start_odometry = False
         
-        # Construct subscriber
+        # Construct publisher
         self.pub_odom = rospy.Publisher(
-            "/odometry",
-            motor_cmd,
+            "/encoder",
+            encoder,
             self.motor_cb,
             buff_size=2**24,
             queue_size=10
         )
-        self.gain, self.trim = self.load_param()
+        
+        GPIO_MOTOR_ENCODER_1=18
+        GPIO_MOTOR_ENCODER_2=19
+        self.driver_L = WheelEncoderDriver(GPIO_MOTOR_ENCODER_1)
+        self.driver_R = WheelEncoderDriver(GPIO_MOTOR_ENCODER_2)
+
+        self.config = self.load_param()
 
         self.first_image_received = False
         self.initialized = True
         rospy.loginfo("odem node initialized!")
+        self.timer = rospy.Timer(rospy.Duration(0.10), self.read_encoder) # publishing 10 Hz
 
 
-
-    def odometry(self,L_ticks,L_ticks_prev,R_ticks,R_ticks_prev):
+    def read_encoder(self,event):
+        msg = encoder()
+        msg.enc_L.data = self.driver_L._ticks
+        msg.enc_L.data = self.driver_R._ticks
+        self.pub_odom.publish(msg)
+    
+    def odometry(self,L_ticks,R_ticks,direction_left,direction_right):
+        msg = Odometry()
         
-        alpha = 2 * np.pi / self.encoder_resolution 
+        # Difference in encoder tics from previous measurement                
+        delta_ticks_left = L_ticks - self.L_ticks_prev
+        delta_ticks_right = R_ticks - self.R_ticks_prev
 
+        # Edge case for rotation in the same position
+        if direction_left < 0:
+            delta_ticks_left=delta_ticks_left*-1
+        if direction_right < 0:
+            delta_ticks_right=delta_ticks_right*-1
 
-        delta_ticks_left = L_ticks - L_ticks_prev
-        delta_ticks_right =
+        # Update previous ticks with new tick values
+        self.L_ticks_prev = L_ticks
+        self.R_ticks_prev = R_ticks
+        
+        # Calculate rotation distance of left and right wheel in radiants
+        rotation_wheel_left = delta_ticks_left * self.alpha 
+        rotation_wheel_right = delta_ticks_right * self.alpha
 
+        # Calculate distance each wheel has travelled since last measurement
+        d_left = self.config["wheel_rad"] * rotation_wheel_left
+        d_right = self.config["wheel_rad"] * rotation_wheel_right
 
-        return xticks, yticks, position
-    def load_params(self):
-        config =
+        ## Average distance travelled by the robot
+        d_A = (d_right + d_left)/2
+
+        ## How much the robot has turned (delta  )
+        d_theta = (d_right - d_left)/(2*self.config["baseline"])
+
+        ## Filling the Odometry message
+        # fill absolute position relative to the origin 
+
+        self.x = self.x + self.config["wheel_rad"] * (rotation_wheel_left + rotation_wheel_right)*np.cos(self.theta)/2
+        self.y = self.y + self.config["wheel_rad"] * (rotation_wheel_left + rotation_wheel_right)*np.sin(self.theta)/2
+        self.theta = self.theta + self.config["wheel_rad"]*(rotation_wheel_right - rotation_wheel_left)/(self.config["baseline"])
+
+        msg.pose.pose.position.x = self.x #x
+        msg.pose.pose.position.y = self.y #y
+        msg.pose.pose.position.z = 0.0  #z is always zeros as we don't drive into the air
+
+        return d_A,d_theta
+    def load_param(self):
+        config = {}
+        config["gain"] = rospy.get_param("~gain")
+        config["trim"] = rospy.get_param("~trim")
+        config["baseline"] = rospy.get_param("~baseline")
+        config["wheel_rad"] = rospy.get_param("~wheel_rad")
+        config["encoder_res"] = rospy.get_param("~encoder_res")
+        config["velocity"] = rospy.get_param("~velocity")
+        
+        rospy.loginfo("Loaded config: %s", config)
+        return config        
 {} 
 if __name__ == "__main__":
     # Initialize the node
-    rospy.init_node('odometry_node', anonymous=True, xmlrpc_port=45102, tcpros_port=45103)
-    odometry_node = MotorSubscriberNode()
+    rospy.init_node('odometry_node')
+    odometry_node = OdometryPublisherNode()
     try:
         rospy.spin()
     except KeyboardInterrupt:
