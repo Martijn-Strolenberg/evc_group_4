a49680d63bd36f8ee2623bb628800867c877d8f2
martijn <m.p.m.strolenberg@student.tue.nl>
2025-05-13 11:08:25 -0500

    removed spaces

--- workshops/workshop3_group4/src/motor_control/src/temp_control_node.py
+++ workshops/workshop3_group4/src/motor_control/src/temp_control_node.py
@@ -1,61 +1,29 @@
 #!/usr/bin/env python2
 
-
-
 import time
-
 import hat
-
 from hat import *
-
 import motorDriver
-
 from motorDriver import DaguWheelsDriver
-
-import cv2
-
 import rospy
-
 import numpy as np
-
-
-
 from sensor_msgs.msg import CompressedImage
-
 from jetson_camera.msg import twovids
-
 from motor_control.msg import motor_cmd,encoder
-
-
-
-
 
 class MotorSubscriberNode:
 
     def __init__(self):
-
         self.initialized = False
-
         rospy.loginfo("Initializing motor control node...")
 
-
-
-
-
         # Construct subscriber
-
         self.sub_cmd = rospy.Subscriber(
-
             "/encoder",
-
             encoder,
-
             self.motor_cb,
-
             buff_size=2**24,
-
             queue_size=10
-
         )
 
         self.gain, self.trim = self.load_param()
@@ -66,13 +34,8 @@ class MotorSubscriberNode:
         rospy.loginfo("motor control node initialized!")
 
 
-
-
-
     def motor_cb(self, data):
-
         if not self.initialized:
-
             return
 
         distance = data.abs_distance 
@@ -102,11 +65,7 @@ class MotorSubscriberNode:
         return gain,trim
 
 
-
-
-
 if __name__ == "__main__":
-
     # Initialize the node
     rospy.init_node('motor_control_node', anonymous=True, xmlrpc_port=45100, tcpros_port=45101)
     camera_node = MotorSubscriberNode()
