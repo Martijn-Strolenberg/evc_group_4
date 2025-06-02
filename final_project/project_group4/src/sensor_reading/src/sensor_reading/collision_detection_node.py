939a8782a85195d65ccddd5144d761dfecb35f2a
Luka Tepavcevic <l.tepavcevic@student.tue.nl>
2025-06-02 11:59:22 +0200

    Collision Detection theoretical code

--- final_project/project_group4/src/sensor_reading/src/sensor_reading/collision_detection_node.py
+++ final_project/project_group4/src/sensor_reading/src/sensor_reading/collision_detection_node.py
@@ -2,6 +2,7 @@
 
 import rospy
 from std_msgs.msg import Float64
+from sensor_reading.srv import CollisionDetection
 
 class CollisionDetectionNode:
  
@@ -20,15 +21,27 @@ class CollisionDetectionNode:
 
         self.initialized = True
         rospy.loginfo("Collision Detection node initialized!")
+        self.state = 0
 
     
-    def set_tof_cb(self, msg):
-        self.tof = msg.data
+    def set_tof_cb(self, data):
+        self.tof = data.data
         rospy.loginfo("TOF distance: %f", self.tof)
+    def state_tof(self):
+        if self.tof <= 200:
+            self.service_tof(True)
+        else:
+            self.service_tof(False)
+    
+    def service_tof(self,collision):
+        try:
+            proxy = rospy.ServiceProxy('col_detect', CollisionDetection)
+            resp = proxy(collision)
+            print("Detected Wall:", resp.success)
+        except rospy.ServiceException as e:
+            print("Service call failed:", e)
 
         
-
-
 if __name__ == "__main__":
     # Initialize the node
     rospy.init_node('collision_detection_node')
